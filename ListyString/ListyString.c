757eca5010f2361fa285d1933585bf4c33ed46a8
Charlton Trezevant <ct@ctis.me>
2018-10-10 21:30:39 -0400

    Initial commit of ListyString files

---
+++ ListyString/ListyString.c
@@ -0,0 +1,132 @@
+/*
+  Charlton Trezevant
+  Sean Szumlanski
+  COP 3502C
+  
+  Assignment #3 - ListyString.c
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include "ListyString.h"
+
+void print_usage(char *name);
+
+int main(int argc, char **argv)
+{
+  // Handle cases where too few arguments are provided, so we don't segfault.
+	if(argc < 2 || argv[2] == NULL){
+		print_usage(argv[0]);
+		return 1;
+	}
+  
+  return processInputFile(argv[1]);
+}
+
+int processInputFile(char *filename)
+{
+  FILE *ifp;
+  char *raw_string, *cmd;
+  int i;
+  ListyString *listy;
+  
+  ifp = fopen(filename, "r");
+  
+  if (ifp == NULL)
+    return 1;
+  
+  fscanf(temperatures, "%f", &currentTemp);
+  
+  listy = createListyString(raw_string);
+  
+  if (listy == NULL)
+    return 1;
+  
+  return 0;
+}
+
+ListyString *createListyString(char *str)
+{
+  ListyString *list;
+  ListyNode *head, *prev_node, *tmp;
+  int raw_len = strlen(str);
+  
+  // May want to double check on how to handle strlen of 0 here.
+  if(raw_len <= 0)
+    return NULL;
+  
+  list = malloc(sizeof(ListyString));
+  list->length = raw_len;
+  
+  list->head = malloc(sizeof(ListyNode));
+  head->data = str[0];
+    
+  for(int i = 1; i < raw_len - 1; i++){
+    tmp = malloc(sizeof(ListyNode));
+    
+    tmp->data = str[i];
+    
+    prev_node->next = tmp;
+    prev_node = tmp;
+  }
+  
+  return list;
+}
+
+ListyString *destroyListyString(ListyString *listy)
+{
+  
+}
+
+ListyString *cloneListyString(ListyString *listy)
+{
+  
+}
+
+void replaceChar(ListyString *listy, char key, char *str)
+{
+  
+}
+
+void reverseListyString(ListyString *listy)
+{
+  
+}
+
+ListyString *listyCat(ListyString *listy, char *str)
+{
+  
+}
+
+int listyCmp(ListyString *listy1, ListyString *listy2)
+{
+  
+}
+
+int listyLength(ListyString *listy)
+{
+  
+}
+
+void printListyString(ListyString *listy)
+{
+  
+}
+
+void print_usage(char *name)
+{
+	printf("Usage: %s  [input file]\n", name);
+}
+
+
+double difficultyRating(void)
+{
+  return 5.0;
+}
+
+
+double hoursSpent(void)
+{
+  return 5.0;
+}
